1f4136f9ba2ac5f639acdbfc823865f1bcaa3b30
jjmarkowski <jjmarkow@comcast.net>
2013-11-07 19:25:27 -0700

    Added interactions to GameApplet.java

--- GameApplet.java
+++ GameApplet.java
@@ -1,12 +1,27 @@
 import java.awt.*;
 import java.applet.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class GameApplet extends Applet {
+public class GameApplet extends JApplet implements ActionListener {
 
 	private CardDisplay[] displayUnshuffle;
 	private CardDisplay[] displayShuffle;
 
+	private JPanel panel = new JPanel();
+	private JButton button = new JButton("Click Me");
+	private JLabel label = new JLabel("Hello");
+
 	public void init() {
+		//this.setLayout(new GridLayout(0, 1));
+
+		//panel.setLayout(new FlowLayout());
+		button.addActionListener(this);
+		button.setActionCommand("Click Me");
+		panel.add(button);
+		panel.add(label);
+
+		this.add(panel);
 
 		displayUnshuffle = new CardDisplay[52];
 		displayShuffle = new CardDisplay[52];
@@ -28,6 +43,7 @@ public class GameApplet extends Applet {
 	}
 
 	public void paint(Graphics g) {
+		super.paint(g);
 		int scale = 20;
 		int x = 50;
 
@@ -40,6 +56,17 @@ public class GameApplet extends Applet {
 		for (int i=0; i<52; i++) {
 			displayShuffle[i].draw(g, new Rectangle(x, 200, scale*2, scale*3));
 			x += scale;
+		}
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Click Me")) {
+			if (label.getText().equals("Hello")) {
+				label.setText("Goodbye");
+			}else {
+				label.setText("Hello");
+			}
 		}
 
 	}
